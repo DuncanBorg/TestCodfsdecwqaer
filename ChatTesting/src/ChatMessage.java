5f79e7a50d33ffe481305c3d953850480aba6840
DuncanBorg <duborg@crimsonwing.com>
2015-12-04 12:41:55 +0100

    Chatmessage  class

---
+++ ChatTesting/src/ChatMessage.java
@@ -0,0 +1,11 @@
+
+public class ChatMessage {
+	public String timestamp;
+	public String content;
+	
+	public void ChatMessage(String time , String con)
+	{
+		timestamp = time;
+		content = con;
+	}
+}
